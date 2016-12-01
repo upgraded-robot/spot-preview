6d1de3dab4a244c3f0416d56caf504f53d95f6d0
upgraded-robot <mariadotdev.ln@gmail.com>
2016-12-01 00:18:59 -0500

    added animation

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 	var template = Handlebars.compile($('#tracks-template').html());
 	$(document).on('click','button', function(e){
+		$('.search').animate({"min-height":"30vh", "font-size":"10pt"}, 500)
 		$('.tracks').empty()
+		$('.previous-search').detach()
 		var q = $('input[name=search]').val()
 		var url = `https://api.spotify.com/v1/search?q=${q}&type=track`
 		$.ajax({
@@ -9,8 +11,13 @@ $(document).ready(function(){
 			url: url
 		}).done(function(data){
 			$('.tracks').append(template({tracks:data.tracks.items}))
+			$('.search-form').append(`<div class="previous-search">Displaying results for: ${q}</div>`)
+			$('input[name=search]').val('')
 		}).fail(function(){
 			console.log('unable to process request')
 		})
+	})
+	$(document).on('focus', 'input', function(e){
+		$('.search').animate({"min-height":"80vh", "font-size":"12pt"}, 1000)
 	})
 })
