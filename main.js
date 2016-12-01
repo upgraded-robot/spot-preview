dd52fcc20bb6e548f454e741f24d38bf932d3ee1
upgraded-robot <mariadotdev.ln@gmail.com>
2016-12-01 12:03:16 -0500

    more final touches

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 $(document).ready(function(){
-	var template = Handlebars.compile($('#tracks-template').html());
+	var template = Handlebars.compile($('#tracks-template').html()); //compile handlbars template
 	$(document).on('keyup','input', function(e){
-		if(e.keyCode == '13'){
-			$('.tracks').empty()
-			$('.previous-search').detach()
-			var q = $('input[name=search]').val()
-			var url = `https://api.spotify.com/v1/search?q=${q}&type=track`
-			$.ajax({
+		if(e.keyCode == '13'){ //
+			$('.tracks').empty() //remove previously found tracks
+			$('.previous-search').detach() //remove previous search term
+			var q = $('input[name=search]').val() //get search term from input
+			var url = `https://api.spotify.com/v1/search?q=${q}&type=track` //spotify api endpoint
+			// ajax call 
+			$.ajax({ 
 				method:'get',
 				url: url
 			}).done(function(data){
 				$('.tracks').append(template({tracks:data.tracks.items}))
-				$('.search-form').append(`<div class="previous-search">Displaying results for: ${q}</div>`)
-				$('input[name=search]').val('')
+				$('.search-form').append(`<div class="previous-search">Displaying results for: ${q}</div>`) //append div showing the searched keyword
+				$('input[name=search]').val('') //empty search input field
 				$('.search').animate({"min-height":"30vh", "font-size":"10pt"}, 500)
 			}).fail(function(){
 				console.log('unable to process request')
