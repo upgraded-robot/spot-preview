5c03fd6be81ff95d66ecbd501151a16216dfeabe
upgraded-robot <mariadotdev.ln@gmail.com>
2016-11-30 23:35:15 -0500

    improvements related to the display of the tracks

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 $(document).ready(function(){
 	var template = Handlebars.compile($('#tracks-template').html());
-	$('button').on('click', function(e){
-		console.log('hey')
-		e.preventDefault()
-		var q = 'muse'
-		var url = `https://api.spotify.com/v1/search?q=${q}&type=track`;
+	$(document).on('click','button', function(e){
+		$('.tracks').empty()
+		var q = $('input[name=search]').val()
+		var url = `https://api.spotify.com/v1/search?q=${q}&type=track`
 		$.ajax({
 			method:'get',
 			url: url
 		}).done(function(data){
-			console.log(data.tracks.items)
 			$('.tracks').append(template({tracks:data.tracks.items}))
 		}).fail(function(){
 			console.log('unable to process request')
